e0809514c9b8d9054391525afc4fffca9cccac20
Gabriel <115580274+gabszs@users.noreply.github.com>
2024-03-15 16:10:24 -0300

    feat(BaseService): Adding the services packge alongside with the first class
    The base_service allow a basic main class for all the services

---
+++ app/services/base_service.py
@@ -0,0 +1,29 @@
+from uuid import UUID
+
+from app.repository.base_repository import BaseRepository
+
+
+class BaseService:
+    def __init__(self, repository: BaseRepository) -> None:
+        self._repository = repository
+
+    def get_list(self, schema):
+        return self._repository.read_by_options(schema)
+
+    def get_by_id(self, id: UUID):
+        return self._repository.read_by_id(id)
+
+    def add(self, schema):
+        return self._repository.create(schema)
+
+    def patch(self, id: UUID, schema):
+        return self._repository.update(id, schema)
+
+    def patch_attr(self, id: UUID, attr: str, value):
+        return self._repository.update_attr(id, attr, value)
+
+    def put_update(self, id: UUID, schema):
+        self._repository.whole_update(id, schema)
+
+    def remove_by_id(self, id: UUID):
+        return self._repository.delete_by_id(id)
