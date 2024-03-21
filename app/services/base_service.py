d0bef095ca3dbd34f1b0dc7cff28cdfc548f6be9
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-03-21 19:11:09 -0300

    refactor(BaseService): Adding async capabnilities in the base_service to allow IO Database actions

--- app/services/base_service.py
+++ app/services/base_service.py
@@ -7,23 +7,23 @@ class BaseService:
     def __init__(self, repository: BaseRepository) -> None:
         self._repository = repository
 
-    def get_list(self, schema):
-        return self._repository.read_by_options(schema)
+    async def get_list(self, schema):
+        return await self._repository.read_by_options(schema)
 
-    def get_by_id(self, id: UUID):
-        return self._repository.read_by_id(id)
+    async def get_by_id(self, id: UUID):
+        return await self._repository.read_by_id(id)
 
-    def add(self, schema):
-        return self._repository.create(schema)
+    async def add(self, schema):
+        return await self._repository.create(schema)
 
-    def patch(self, id: UUID, schema):
-        return self._repository.update(id, schema)
+    async def patch(self, id: UUID, schema):
+        return await self._repository.update(id, schema)
 
-    def patch_attr(self, id: UUID, attr: str, value):
-        return self._repository.update_attr(id, attr, value)
+    async def patch_attr(self, id: UUID, attr: str, value):
+        return await self._repository.update_attr(id, attr, value)
 
-    def put_update(self, id: UUID, schema):
-        self._repository.whole_update(id, schema)
+    async def put_update(self, id: UUID, schema):
+        return await self._repository.whole_update(id, schema)
 
-    def remove_by_id(self, id: UUID):
-        return self._repository.delete_by_id(id)
+    async def remove_by_id(self, id: UUID):
+        return await self._repository.delete_by_id(id)
