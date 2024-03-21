cd5c0f0c238ece1909f75940bf92ae7fcd35aaa5
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-03-21 19:09:19 -0300

    refactor(Usermodel): Adding mapped_column in User model to identify the model as unique

--- app/models/api_models.py
+++ app/models/api_models.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
 
 from app.models.base_model import BaseModel
 
@@ -6,9 +7,9 @@ from app.models.base_model import BaseModel
 class User(BaseModel):
     __tablename__ = "users"
 
-    username: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
+    username: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str]
-    email: Mapped[str]
 
     # resumes: Mapped[list["Resume"]] = relationship(
     #     back_populates="user", cascade="all, delete-orphan"
--- app/models/base_model.py
+++ app/models/base_model.py
@@ -17,6 +17,8 @@ def generate_uuid4() -> str:
 
 
 class BaseModel(MappedAsDataclass, DeclarativeBase):
-    id: Mapped[UUID] = mapped_column(types.Uuid, primary_key=True, init=False, server_default=text("gen_random_uuid()"))
-    created_at = Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    id: Mapped[UUID] = mapped_column(
+        types.Uuid, primary_key=True, init=False, server_default=text("gen_random_uuid()"), unique=True
+    )
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
