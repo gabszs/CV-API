a22b0e2f87379800b86db469163bee51440548ea
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-03-21 19:04:24 -0300

    feat(Database): Adding the dynamic url setting in the database class, also creating a func to get_session to use as depency injector of the sessions

--- app/core/database.py
+++ app/core/database.py
@@ -1,15 +1,17 @@
 from contextlib import AbstractContextManager
-from contextlib import contextmanager
+from contextlib import asynccontextmanager
 from typing import Any
 from typing import Callable
 
 from sqlalchemy import create_engine
 from sqlalchemy import orm
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.declarative import as_declarative
 from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.orm import Session
 from sqlalchemy.sql.expression import and_
 
+from app.core.settings import Settings
 
 SQLALCHEMY_QUERY_MAPPER = {
     "eq": "__eq__",
@@ -32,25 +34,31 @@ class BaseModel:
 
 
 class Database:
-    def __init__(self, db_url: str) -> None:
+    def __init__(self, db_url: str = Settings().DATABASE_URL) -> None:
         self._engine = create_engine(db_url, echo=True)
         self._session_factory = orm.scoped_session(
-            orm.sessionmaker(autocommit=False, autoflush=False, bind=self._engine)
+            orm.sessionmaker(autocommit=False, autoflush=False, bind=self._engine, class_=AsyncSession)
         )
 
     def create_database(self) -> None:
         BaseModel.metadata.create_all(self._engine)
 
-    @contextmanager
-    def session(self) -> Callable[..., AbstractContextManager[Session]]:
-        session: Session = self._session_factory()
+    @asynccontextmanager
+    async def session(self) -> Callable[..., AbstractContextManager[Session]]:
+        session: Session = await self._session_factory()
         try:
             yield session
         except Exception:
-            session.rollback()
+            await session.rollback()
             raise
         finally:
-            session.close()
+            await session.close()
+
+
+async def get_session() -> Session:
+    db = Database()
+    async with db.session() as session:
+        yield session
 
 
 def dict_to_sqlalchemy_filter_options(model_class, search_option_dict):
