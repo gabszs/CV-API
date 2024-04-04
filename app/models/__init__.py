87c85c81522e2a0c089bdbc85f77ce7f95651544
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-04-04 15:37:56 -0300

    fix(Models): Changing the baseModel of init models from old BaselModel to new and correct BaseModel

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,19 +1,5 @@
-from uuid import UUID
-
-from sqlalchemy.ext.declarative import declared_attr
-from sqlalchemy.orm import as_declarative
-
 from .api_models import User
-
-
-@as_declarative()
-class BaseModel:
-    id: UUID
-    __name__: str
-
-    @declared_attr
-    def __table__(cls):
-        return cls.__name__.lower()
+from .base_model import BaseModel
 
 
 __all__ = ["User", "BaseModel"]
