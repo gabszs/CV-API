e15e7012a7c9fb21175f63bb18559f3daf52d51d
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-04-06 00:19:56 -0300

    test(Conftest): Changin the conftest fixture by remving the pytest-postgresql

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,36 +1,29 @@
 import asyncio
+from asyncio import WindowsSelectorEventLoopPolicy  # noqa
 from contextlib import ExitStack
 
 import pytest
 import pytest_asyncio
 from fastapi.testclient import TestClient
-from icecream import ic
-from pytest_postgresql import factories
-from pytest_postgresql.janitor import DatabaseJanitor
-from sqlalchemy import select
+from pytest_postgresql import factories  # noqa
+from pytest_postgresql.janitor import DatabaseJanitor  # noqa
+from sqlalchemy import select  # noqa
+from sqlalchemy.testing.entities import ComparableEntity  # noqa
 
 from app.core.database import get_db
 from app.core.database import sessionmanager
+from app.core.settings import settings
 from app.main import init_app
-from app.models import User
+from app.models import User  # noqa
 from tests.factories import UserFactory
-# from sqlalchemy.engine import create_engine
-# from app.models import Base
 
 
-test_db = factories.postgresql_noproc(user="app_user", password="app_password", host="localhost", port="5432")
-
-
-async def show_users_in_table(session):
-    all_users = await session.execute(select(User))
-    print("Todos os usuários antes de adicionar:")
-    for user_row in all_users:
-        ic(user_row)
-
-
-@pytest.fixture
-def user_factory():
-    return UserFactory()
+@pytest.fixture(scope="session")
+def event_loop(request):
+    asyncio.set_event_loop_policy(WindowsSelectorEventLoopPolicy())
+    loop = asyncio.get_event_loop()
+    yield loop
+    loop.close()
 
 
 @pytest.fixture(autouse=True)
@@ -41,72 +34,35 @@ def app():
 
 @pytest.fixture
 def client(app):
-    with TestClient(app) as client:
-        yield client
-
-
-@pytest.fixture(scope="session")
-def event_loop(request):
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    loop = asyncio.get_event_loop_policy().new_event_loop()
-    yield loop
-    loop.close()
-
-
-@pytest.fixture(scope="session", autouse=True)
-async def connection_test(test_db, event_loop):
-    pg_host = test_db.host
-    pg_port = test_db.port
-    pg_user = test_db.user
-    pg_db = test_db.dbname
-    pg_password = test_db.password
-    pg_version = test_db.version
-
-    with DatabaseJanitor(pg_user, pg_host, pg_port, pg_db, pg_version, pg_password):
-        connection_str = f"postgresql+psycopg://{pg_user}:@{pg_host}:{pg_port}/{pg_db}"
-        sessionmanager.init(connection_str)
-        # engine = create_engine(connection_str)
-        # with engine.connect() as conn:
-        #     Base.metadata.create_all(conn)
-        yield
-        await sessionmanager.close()
-
-
-@pytest.fixture(scope="function", autouse=True)
-async def create_tables(connection_test):
-    async with sessionmanager.connect() as connection:
-        await sessionmanager.drop_all(connection)
-        await sessionmanager.create_all(connection)
-        # await sessionmanager.drop_all_from_base(connection, Base)
-        # await sessionmanager.create_all_from_base(connection, Base)
-
-
-@pytest.fixture(scope="function", autouse=True)
-async def session_override(app, connection_test):
-    async def get_db_override():
-        async with sessionmanager.session() as session:
-            yield session
-
-    app.dependency_overrides[get_db] = get_db_override
+    with TestClient(app) as c:
+        yield c
 
 
 @pytest_asyncio.fixture(scope="function")
-async def session(connection_test):
-    async with sessionmanager.session() as session:
+async def db(event_loop):
+    sessionmanager.init(settings.TEST_DATABASE_URL)
+    async with sessionmanager.connect() as conn:
+        await sessionmanager.drop_all(conn)
+        await sessionmanager.create_all(conn)
+    yield sessionmanager
+    async with sessionmanager.connect() as conn:
+        await sessionmanager.drop_all(conn)
+    await sessionmanager.close()
+
+
+@pytest_asyncio.fixture(scope="function")
+async def session(db):
+    async with db.session() as session:
         yield session
 
 
 @pytest_asyncio.fixture()
 async def user(session):
-    # await show_users_in_table(session)
     user = UserFactory()
     session.add(user)
     await session.commit()
     await session.refresh(user)
-    yield user
-
-    await session.delete(user)
-    await session.commit()
+    return user
 
 
 @pytest_asyncio.fixture()
@@ -115,7 +71,13 @@ async def other_user(session):
     session.add(other_user)
     await session.commit()
     await session.refresh(other_user)
-    yield other_user
+    return other_user
 
-    await session.delete(other_user)
-    await session.commit()
+
+@pytest.fixture(scope="function")
+async def session_override(app, connection_test):
+    async def get_db_override():
+        async with sessionmanager.session() as session:
+            yield session
+
+    app.dependency_overrides[get_db] = get_db_override
