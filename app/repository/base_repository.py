46c27f8b667a2bb133f0859a9ca311e969dc81c8
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-04-15 18:34:52 -0300

    refactor(BaseRepository): Changing the base repository method get_by_id to simply code and more easy to uderstang code

--- app/repository/base_repository.py
+++ app/repository/base_repository.py
@@ -3,7 +3,6 @@ from typing import Callable
 from uuid import UUID
 
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm import joinedload
 from sqlalchemy.orm import Session
 
 from app.core.database import dict_to_sqlalchemy_filter_options
@@ -19,7 +18,7 @@ class BaseRepository:
         self.session_factory = session_factory
         self.model = model
 
-    async def read_by_options(self, schema, eager=False):
+    async def read_by_options(self, schema):
         async with self.session_factory() as session:
             schema_as_dict = schema.dict(exclude_none=True)
             ordering = schema_as_dict.get("ordering", settings.ORDERING)
@@ -32,9 +31,7 @@ class BaseRepository:
             page_size = schema_as_dict.get("page_size", settings.PAGE_SIZE)
             filter_options = dict_to_sqlalchemy_filter_options(self.model, schema.dict(exclude_none=True))
             query = await session.query(self.model)
-            if eager:
-                for eager in getattr(self.model, "eagers", []):
-                    query = query.options(joinedload(getattr(self.model, eager)))
+
             filtered_query = query.filter(filter_options)
             query = filtered_query.order_by(order_query)
             if page_size == "all":
@@ -52,22 +49,19 @@ class BaseRepository:
                 },
             }
 
-    async def read_by_id(self, id: UUID, eager=False):
+    async def read_by_id(self, id: UUID):
         async with self.session_factory() as session:
-            query = await session.query(self.model)
-            if eager:
-                for eager in getattr(self.model, "eagers", []):
-                    query = query.options(joinedload(getattr(self.model, eager)))
-            query = query.filter(self.model.id == id).first()
-            if not query:
-                raise NotFoundError(detail=f"not found id : {id}")
-            return query
+            result = await session.get(self.model, id)
+            if not result:
+                raise NotFoundError(detail=f"id not found: {id}")
+            return result
 
     async def create(self, schema):
         async with self.session_factory() as session:
+            print(self.model, schema)
             query = self.model(**schema.dict())
             try:
-                await session.add(query)
+                session.add(query)
                 await session.commit()
                 await session.refresh(query)
             except IntegrityError as e:
