7be2341d5a71aa134a09c5ba6eb821cc036ce426
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-04-17 17:26:52 -0300

    fix(Update): Chandfing the update method

--- app/repository/base_repository.py
+++ app/repository/base_repository.py
@@ -3,6 +3,7 @@ from typing import Callable
 from uuid import UUID
 
 from sqlalchemy import select
+from sqlalchemy import update
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
@@ -10,7 +11,6 @@ from app.core.exceptions import DuplicatedError
 from app.core.exceptions import NotFoundError
 from app.core.settings import Settings
 from app.schemas.base_schema import FindBase
-from app.schemas.user_schema import FindUserResult
 
 settings = Settings()
 
@@ -24,24 +24,18 @@ class BaseRepository:
         async with self.session_factory() as session:
             query = await session.execute(select(self.model).offset(schema.offset).limit(schema.limit))
             result = query.scalars().all()
-            return FindUserResult(
-                founds=result,
-                search_options={
-                    "offset": schema.offset,
-                    "limit": schema.limit,
-                },
-            )
+            return result
 
     async def read_by_id(self, id: UUID):
         async with self.session_factory() as session:
             result = await session.get(self.model, id)
+
             if not result:
                 raise NotFoundError(detail=f"id not found: {id}")
             return result
 
     async def create(self, schema):
         async with self.session_factory() as session:
-            print(self.model, schema)
             query = self.model(**schema.dict())
             try:
                 session.add(query)
@@ -53,9 +47,20 @@ class BaseRepository:
 
     async def update(self, id: UUID, schema):
         async with self.session_factory() as session:
-            await session.query(self.model).filter(self.model.id == id).update(schema.dict(exclude_none=True))
-            await session.commit()
-            return self.read_by_id(id)
+            result = await session.get(self.model, id)
+            if not result:
+                raise NotFoundError(detail=f"id not found: {id}")
+
+            stmt = update(self.model).where(self.model.id == id).values(**schema.model_dump())
+            print(f"stmt: {stmt}")
+            try:
+                await session.execute(stmt)
+
+            except IntegrityError as e:
+                error_message = ":".join(str(e.orig).replace("\n", " ").split(":")[1:])
+                raise DuplicatedError(detail=error_message)
+
+            return result
 
     async def update_attr(self, id: UUID, column: str, value):
         async with self.session_factory() as session:
