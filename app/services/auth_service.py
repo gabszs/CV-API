06d4414d9d779cdbf1c6045c0cb0433a727333d7
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-04-18 19:04:41 -0300

    fix(PayloadScheme): Changing the type of UUID ID to str by force to allow json_dumps

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -22,11 +22,7 @@ class AuthService(BaseService):
         super().__init__(user_repository)
 
     async def sign_in(self, sign_in_info: SignIn):
-        # find_user = FindUser()
-        # find_user.email__eq = sign_in_info.email__eq
-        # user: List[User] = await self.user_repository.read_by_options(find_user)["founds"]
         user: List[User] = await self.user_repository.read_by_email(email=sign_in_info.email__eq)
-        user
         if len(user) < 1:
             raise AuthError(detail="Incorrect email or password")
         found_user = user[0]
@@ -36,11 +32,9 @@ class AuthService(BaseService):
 
         delattr(found_user, "password")
 
-        payload = Payload(id=found_user.id, email=found_user.email, username=found_user.username)
-
+        payload = Payload(id=str(found_user.id), email=found_user.email, username=found_user.username)
         token_lifespan = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
         access_token, expiration_datetime = create_access_token(payload.model_dump(), token_lifespan)
-        # sign_in_result = {"access_token": access_token, "expiration": expiration_datetime, "user_info": found_user}
         sign_in_result = SignInResponse(access_token=access_token, expiration=expiration_datetime, user_info=found_user)
         return sign_in_result
 
