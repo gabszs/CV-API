0ed4e53d5389b29a1212511c0f229966101c6436
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-04-18 19:01:11 -0300

    fix(Settings): Change the typ[e fo access_token_expuire to correcth type hint, int instead of str

--- app/core/settings.py
+++ app/core/settings.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
 
     SECRET_KEY: str
     ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
