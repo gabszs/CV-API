775f9665a64360f0372906b2068cf52655b29ce2
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-04-23 19:07:41 -0300

    feat(Settings): Adding load_env due to a bug

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,12 +1,15 @@
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 from pydantic_settings import SettingsConfigDict
 
 
+load_dotenv()
+
+
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file_encoding="utf-8")
 
     DATABASE_URL: str
-    TEST_DATABASE_URL: str
 
     SECRET_KEY: str
     ALGORITHM: str
@@ -21,7 +24,8 @@ class Settings(BaseSettings):
     ORDERING: str
 
     DATETIME_FORMAT: str = "%Y-%m-%dT%H:%M:%S"
-
+    TEST_DATABASE_URL: str
+    # TEST_DATABASE_URL: str = "postgresql+asyncpg://app_user:app_password@localhost:5433/app_db"
     OPENAI_API_KEY: str
 
 
