132312bbd9d0a9275b5dff3c1889a3b31503a9e0
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-04-23 18:58:15 -0300

    feat(UsersRoute): Adding new delete-disable route, also correcting the model_response at put route

--- app/routes/v1/users_routes.py
+++ app/routes/v1/users_routes.py
@@ -2,6 +2,7 @@ from uuid import UUID
 
 from fastapi import APIRouter
 
+from app.core.dependencies import CurrentUserDependency
 from app.core.dependencies import UserServiceDependency
 from app.schemas.base_schema import FindBase
 from app.schemas.base_schema import Message
@@ -11,15 +12,14 @@ from app.schemas.user_schema import FindUserResult
 from app.schemas.user_schema import UpsertUser
 from app.schemas.user_schema import User
 
-from app.core.dependencies import CurrentUserDependency
-
 
 router = APIRouter(prefix="/user", tags=["user"])
 
 
-@router.get("", response_model=FindUserResult)
+@router.get("/", response_model=FindUserResult)
 async def get_user_list(service: UserServiceDependency, offset: int = 0, limit: int = 100):
     users = await service.get_list(FindBase(offset=offset, limit=limit))
+    print(users)
     return FindUserResult(
         founds=users, search_options=SearchOptions(offset=offset, limit=limit, total_count=len(users))
     )
@@ -30,14 +30,28 @@ async def get_user_by_id(user_id: UUID, service: UserServiceDependency):
     return await service.get_by_id(user_id)
 
 
-@router.post("", status_code=201, response_model=User)
+@router.post("/", status_code=201, response_model=User)
 async def create_user(user: BaseUserWithPassword, service: UserServiceDependency):
     return await service.add(user)
 
 
-@router.put("/{user_id}")
-async def update_user(user_id: UUID, user: UpsertUser, service: UserServiceDependency, current_user: CurrentUserDependency):
+### adicionar validacao para quano o a request tiver parametros iguais ao do current_user
+@router.put("/{user_id}", response_model=User)
+async def update_user(
+    user_id: UUID, user: UpsertUser, service: UserServiceDependency, current_user: CurrentUserDependency
+):
     return await service.patch(id=user_id, schema=user, current_user=current_user)
+
+
+# @router.patch("/change_password/{user_id}")
+# async def change_password(user_id: UUID, service: UserServiceDependency, current_user: CurrentUserDependency):
+#     return await service.patch_attr(id=user_id, attr="password")
+
+
+@router.delete("/disable/{user_id}", response_model=Message)
+async def disable_user(user_id: UUID, service: UserServiceDependency, current_user: CurrentUserDependency):
+    await service.patch_attr(id=user_id, attr="is_active", value=False)
+    return Message(detail="User has been desabled successfully")
 
 
 @router.delete("/{user_id}", response_model=Message)
