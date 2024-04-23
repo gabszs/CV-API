28252a8410d2d30587d6b784f15b9c4b8f051549
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-04-23 18:59:27 -0300

    refactor(Conftest): Refactoring all the conftest to allow tests, the old one are bugged, adding new one

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,113 +1,117 @@
-import asyncio
-import sys
-from asyncio import WindowsSelectorEventLoopPolicy
-from contextlib import ExitStack
+from typing import AsyncGenerator
+from typing import Generator
 
 import pytest
-import pytest_asyncio
-import sqlalchemy as sa
-from fastapi.testclient import TestClient
+from httpx import AsyncClient
+from sqlalchemy import create_engine
+from sqlalchemy import event
+from sqlalchemy import text
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import Session
+from sqlalchemy.orm import SessionTransaction
 
-from app.core.database import get_db
-from app.core.database import sessionmanager
+from app.core.database import get_session_factory
 from app.core.settings import settings
-from app.main import init_app
-from app.models import User
-from tests.factories import UserFactory
+from app.main import app
+from app.models import Base
 
 
-@pytest.fixture(scope="session")
-def event_loop():
-    """
-    Creates an instance of the default event loop for the test session.
-    """
-    if sys.platform.startswith("win") and sys.version_info[:2] >= (3, 8):
-        # Avoid "RuntimeError: Event loop is closed" on Windows when tearing down tests
-        # https://github.com/encode/httpx/issues/914
-        asyncio.set_event_loop_policy(WindowsSelectorEventLoopPolicy())
-
-    loop = asyncio.new_event_loop()
-    yield loop
-    loop.close()
+sync_db_url = settings.TEST_DATABASE_URL.replace("+asyncpg", "")
 
 
 @pytest.fixture
-def user_factory():
-    return UserFactory()
-
-
-@pytest.fixture(scope="session")
-def _database_url():
-    return settings.TEST_DATABASE_URL
-
-
-@pytest.fixture(scope="session")
-def init_database():
-    from app.models import Base
-
-    return Base.metadata.create_all
-
-
-@pytest.fixture(autouse=True)
-def app():
-    with ExitStack():
-        yield init_app(init_db=False)
+def anyio_backend() -> str:
+    return "asyncio"
 
 
 @pytest.fixture
-def client(app):
-    with TestClient(app) as client:
+async def client() -> AsyncGenerator:
+    async with AsyncClient(app=app, base_url="https://test") as client:
         yield client
 
 
-@pytest_asyncio.fixture(scope="function")
-async def db(event_loop, _database_url):
-    sessionmanager.init(_database_url)
-    async with sessionmanager.connect() as conn:
-        await sessionmanager.drop_all(conn)
-        await sessionmanager.create_all(conn)
-    yield sessionmanager
-    async with sessionmanager.connect() as conn:
-        await sessionmanager.drop_all(conn)
-    await sessionmanager.close()
+@pytest.fixture(scope="session")
+def setup_db() -> Generator:
+    engine = create_engine(sync_db_url)
+    conn = engine.connect()
+    conn.execute(text("commit"))
+
+    try:
+        conn.execute(text("drop database test"))
+    except SQLAlchemyError:
+        pass
+    finally:
+        conn.close()
+
+    conn = engine.connect()
+
+    conn.execute(text("commit"))
+    conn.execute(text("create database test"))
+    conn.close()
+
+    yield
+
+    conn = engine.connect()
+    conn.execute(text("commit"))
+    try:
+        conn.execute(text("drop database test"))
+    except SQLAlchemyError:
+        pass
+    conn.close()
+    engine.dispose()
 
 
-@pytest_asyncio.fixture()
-async def session(db):
-    # async with AsyncSession(sessionmanager._engine) as session:
-    #     yield session
-    async with sessionmanager.session() as session:
-        yield session
+@pytest.fixture(scope="session", autouse=True)
+def setup_test_db(setup_db: Generator) -> Generator:
+    engine = create_engine(sync_db_url)
+
+    with engine.begin():
+        Base.metadata.drop_all(engine)
+        Base.metadata.create_all(engine)
+        yield
+        Base.metadata.drop_all(engine)
+
+    engine.dispose()
 
 
-@pytest_asyncio.fixture()
-async def user(session):
-    user = UserFactory()
-    session.add(user)
-    await session.commit()
-    result = await session.execute(sa.select(User))
-    return result.scalars().all()[0]
+@pytest.fixture
+async def session() -> AsyncGenerator:
+    async_engine = create_async_engine(settings.TEST_DATABASE_URL)
+    async with async_engine.connect() as conn:
+        await conn.begin()
+        await conn.begin_nested()
+        AsyncSessionLocal = async_sessionmaker(
+            autocommit=False,
+            autoflush=False,
+            bind=conn,
+            future=True,
+        )
+
+        async_session = AsyncSessionLocal()
+
+        @event.listens_for(async_session.sync_session, "after_transaction_end")
+        def end_savepoint(session: Session, transaction: SessionTransaction) -> None:
+            if conn.closed:
+                return
+            if not conn.in_nested_transaction():
+                if conn.sync_connection:
+                    conn.sync_connection.begin_nested()
+
+        def test_get_session() -> Generator:
+            try:
+                yield AsyncSessionLocal
+            except SQLAlchemyError:
+                pass
+
+        app.dependency_overrides[get_session_factory] = test_get_session
+
+        yield async_session
+        await async_session.close()
+        await conn.rollback()
+
+    await async_engine.dispose()
 
 
-@pytest_asyncio.fixture()
-async def other_user(session):
-    other_user = UserFactory()
-    session.add(other_user)
-    await session.commit()
-    result = await session.execute(sa.select(User))
-    return result.scalars().all()[1]
-
-
-@pytest_asyncio.fixture()
-async def all_users(session):
-    result = await session.execute(sa.select(User))
-    return result.scalars().all()
-
-
-@pytest.fixture(scope="function")
-async def session_override(app, connection_test):
-    async def get_db_override():
-        async with sessionmanager.session() as session:
-            yield session
-
-    app.dependency_overrides[get_db] = get_db_override
+#
