00d8df9a9945497666dc9a416c5522e43562bef0
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-04-24 18:31:19 -0300

    test(SetupUsers): Adding new method to users_factory to allow dynmic create of differente type of users, also refactoring the setup_users at users_route tes to allow comparison between two differentes types of model with clean password

--- tests/factories.py
+++ tests/factories.py
@@ -5,6 +5,7 @@ import factory
 
 from app.core.security import get_password_hash
 from app.models import User
+from app.schemas.user_schema import UserWithCleanPassword
 
 
 def factory_object_to_dict(factory_instance):
@@ -26,11 +27,39 @@ class UserFactory(factory.Factory):
 
 
 def create_factory_users(users_qty: int, password: str = "_password", **kwargs) -> Dict[UserFactory, str]:
-    users_dict: Dict[Dict[str, List[UserFactory]], Dict[str, List[str]]] = {"users": list(), "password_list": list()}
+    users_dict: Dict[Dict[str, List[UserFactory]], Dict[str, List[str]]] = {"users": list(), "clean_users": list()}
     for count in range(users_qty):
         clean_password = f"{count}{password}"
         user = UserFactory(password=get_password_hash(clean_password), **kwargs)
+        clean_user = UserWithCleanPassword(
+            email=user.email, username=user.email, password=user.password, clean_password=clean_password
+        )
 
         users_dict["users"].append(user)
-        users_dict["password_list"].append(clean_password)
+        users_dict["clean_users"].append(clean_user)
     return users_dict
+
+
+def batch_users_by_options(
+    normal_users: int = 0, admin_users: int = 0, disable_users: int = 0, disable_admins: int = 0
+):
+    normal_users = create_factory_users(normal_users)
+    admin_users = create_factory_users(admin_users, is_superuser=True)
+    disable_users = create_factory_users(disable_users, is_active=False)
+    disable_admins = create_factory_users(disable_admins, is_active=False, is_superuser=True)
+    users = [
+        user
+        for user_list in [normal_users["users"], admin_users["users"], disable_users["users"], disable_admins["users"]]
+        for user in user_list
+    ]
+    clean_users = [
+        user
+        for user_list in [
+            normal_users["clean_users"],
+            admin_users["clean_users"],
+            disable_users["clean_users"],
+            disable_admins["clean_users"],
+        ]
+        for user in user_list
+    ]
+    return users, clean_users
--- tests/routes/v1/test_user_route.py
+++ tests/routes/v1/test_user_route.py
@@ -2,57 +2,30 @@ import pytest
 from icecream import ic
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from tests.factories import create_factory_users
+from tests.factories import batch_users_by_options
 
 
-async def setup_data(session: AsyncSession) -> None:
-    users_with_password = create_factory_users(2)
-    ic(users_with_password)
-    session.add_all(users_with_password["users"])
+async def setup_data(
+    session: AsyncSession, normal_users: int = 0, admin_users: int = 0, disable_users: int = 0, disable_admins: int = 0
+) -> None:
+    users, clean_users = batch_users_by_options(
+        normal_users=normal_users, admin_users=admin_users, disable_users=disable_users, disable_admins=disable_admins
+    )
+    session.add_all(users)
     await session.flush()
     await session.commit()
-    return users_with_password
+    return clean_users
+
+
+# @pytest.mark.anyio
+# async def test_setup_data(session, client):
+#     await setup_data(session, admin_users=1, disable_users=5)
+#     users = await client.get("/v1/user/?offset=0&limit=100")
+#     ic(client, users.json())
 
 
 @pytest.mark.anyio
-async def test_setup_data(session):
-    from app.models import User
-    from sqlalchemy import select
-
-    ic(session)
-    t = await setup_data(session)
-    query = await session.execute(select(User))
-    result = query.scalars().all()
-    ic(result, t)
-
-
-@pytest.mark.anyio
-async def test_setup_data2(session):
-    from app.models import User
-    from sqlalchemy import select
-
-    ic(session)
-    t = await setup_data(session)
-    query = await session.execute(select(User))
-    result = query.scalars().all()
-    ic(result, t)
-
-
-@pytest.mark.anyio
-async def test_setup_data3(session):
-    from app.models import User
-    from sqlalchemy import select
-
-    ic(session)
-    t = await setup_data(session)
-    query = await session.execute(select(User))
-    result = query.scalars().all()
-    ic(result, t)
-
-def test_create_user_should_return_201_success(client, db, user_factory):
-    response = client.post(
-        "/v1/user",
-        json={"email": user_factory.email, "username": user_factory.username, "password": user_factory.password},
-    )
-    assert response.status_code == 201
-    print(response.json())
+async def test_setup_data1(session, client):
+    clean_users = await setup_data(session, disable_admins=1)
+    users = await client.get("/v1/user/?offset=0&limit=100")
+    ic(client, users.json(), clean_users)
