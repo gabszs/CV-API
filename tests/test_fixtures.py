c330a994eeb4c849b57f117e19fb1f1a60831e41
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-04-25 19:09:16 -0300

    fix(FixtureTests): Removing the old broken tests of fixturestestes

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -1,38 +1,7 @@
-import pytest
-from fastapi import FastAPI
-from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from app.models import User
-
-
-@pytest.mark.asyncio
-async def test_session_query(session):
-    assert isinstance(session, AsyncSession)
-
-    result = await session.execute(select(User))
-    users = result.scalars().all()
-
-    assert len(users) == 0
-
-
-@pytest.mark.asyncio
-async def test_session_with_users(session, user, other_user):
-    assert isinstance(session, AsyncSession)
-
-    result = await session.execute(select(User))
-    users = result.scalars().all()
-
-    assert len(users) == 2
-
-
-def test_app_fixture(app):
-    assert isinstance(app, FastAPI)
-    assert app.title == "CV-Api"
-    assert app.description == "CV Managment Web api with basic auth crud built by @GabrielCarvalho to my girlfriend"
-    assert app.contact["name"] == "Gabriel Carvalho"
-    assert app.contact["url"] == "https://www.linkedin.com/in/gabzsz/"
-    assert app.contact["email"] == "gabriel.carvalho@huawei.com"
-    assert (
-        app.summary == "WebApi build on best market practices such as TDD, Clean Arch, Data Validation with Pydantic V2"
-    )
+# import pytest
+# from icecream import ic
+# @pytest.mark.asyncio
+# async def test1(session, client):
+#     await setup_users(session)
+#     response = client.get("/v1/user")
+#     ic(response, response.json())
