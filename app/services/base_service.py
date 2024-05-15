d674987897cf37ff7c18cc62a8b7f5726bdd07a1
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-05-15 18:40:50 -0300

    refactor(Userservice): Adding the int type in the base_service methods and adding moving the remove_by_id from incorrected domain base_repo to the correct related user_repo

--- app/services/base_service.py
+++ app/services/base_service.py
@@ -1,3 +1,4 @@
+from typing import Union
 from uuid import UUID
 
 from app.core.exceptions import AuthError
@@ -10,30 +11,33 @@ class BaseService:
     def __init__(self, repository: BaseRepository) -> None:
         self._repository = repository
 
-    async def validate_permission(self, id: UUID, current_user: UserModel):
-        if id != current_user.id:
-            raise AuthError(detail="Not enough permissions")
+    async def validate_permission(self, id: Union[UUID, int], current_user: UserModel, admin: bool = False):
+        if admin:
+            if current_user.is_superuser is not True:
+                raise AuthError(detail="Not enough permissions")
+        else:
+            if id != current_user.id:
+                raise AuthError(detail="Not enough permissions")
 
     async def get_list(self, schema: FindBase):
-        return await self._repository.read_by_options(schema)
+        return await self._repository.read_by_options(schema, eager=True)
 
-    async def get_by_id(self, id: UUID):
+    async def get_by_id(self, id: Union[UUID, int]):
         return await self._repository.read_by_id(id)
 
     async def add(self, schema):
         return await self._repository.create(schema)
 
-    async def patch(self, id: UUID, schema, current_user: UserModel):
+    async def patch(self, id: Union[UUID, int], schema, current_user: UserModel):
         await self.validate_permission(id=id, current_user=current_user)
         return await self._repository.update(id, schema)
 
-    async def patch_attr(self, id: UUID, attr: str, value, current_user: UserModel):
+    async def patch_attr(self, id: Union[UUID, int], attr: str, value, current_user: UserModel):
         await self.validate_permission(id=id, current_user=current_user)
         return await self._repository.update_attr(id, attr, value)
 
-    async def put_update(self, id: UUID, schema):
+    async def put_update(self, id: Union[UUID, int], schema):
         return await self._repository.whole_update(id, schema)
 
-    async def remove_by_id(self, id: UUID, current_user: UserModel):
-        await self.validate_permission(id=id, current_user=current_user)
+    async def remove_by_id(self, id: Union[UUID, int]):
         return await self._repository.delete_by_id(id)
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,4 +1,8 @@
+from typing import Union
+from uuid import UUID
+
 from app.core.security import get_password_hash
+from app.models import User as UserModel
 from app.repository.user_repository import UserRepository
 from app.schemas.user_schema import BaseUserWithPassword
 from app.services.base_service import BaseService
@@ -14,3 +18,7 @@ class UserService(BaseService):
         created_user = await self._repository.create(user_schema)
         delattr(created_user, "password")
         return created_user
+
+    async def remove_by_id(self, id: Union[UUID, int], current_user: UserModel):
+        await self.validate_permission(id=id, current_user=current_user)
+        return await self._repository.delete_by_id(id)
