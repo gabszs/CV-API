4a96652fcd293e81397cfb1e5fb0d522319c83f2
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-05-17 17:20:17 -0300

    test(SkillRoute): Refactoring the tests from get_all tests to new query_find parameteers, but alsso adding the admin_token_header to necessary testes

--- tests/routes/v1/test_skill_route.py
+++ tests/routes/v1/test_skill_route.py
@@ -1,6 +1,9 @@
+from urllib.parse import urlencode
+
 import pytest
 from icecream import ic
 
+from tests.conftest import get_admin_token_header
 from tests.conftest import setup_skill_data
 from tests.conftest import token
 from tests.conftest import validate_datetime
@@ -9,15 +12,17 @@ base_url = "/v1/skill"
 
 
 async def get_skill_by_index(client, index: int):
-    response = await client.get(f"{base_url}/?offset=0&limit=100")
+    response = await client.get(f"{base_url}/?ordering=created_at")
     return response.json()["founds"][index]
 
 
 @pytest.mark.anyio
 async def test_create_normal_skills_should_return_201_POST(client, session, factory_skill):
+    token_header = await get_admin_token_header(client, session)
     response = await client.post(
         f"{base_url}/",
         json={"skill_name": factory_skill.skill_name, "category": factory_skill.category},
+        headers=token_header,
     )
     response_json = response.json()
 
@@ -29,21 +34,22 @@ async def test_create_normal_skills_should_return_201_POST(client, session, fact
 
 
 @pytest.mark.anyio
-async def test_create_normal_skill_should_return_422_unprocessable_entity_POST(client):
-    response = await client.post(
-        f"{base_url}/",
-    )
+async def test_create_normal_skill_should_return_422_unprocessable_entity_POST(client, session):
+    token_header = await get_admin_token_header(client, session)
+    response = await client.post(f"{base_url}/", headers=token_header)
 
     assert response.status_code == 422
 
 
 @pytest.mark.anyio
 async def test_create_normal_skill_should_return_409_email_already_registered_POST(client, session):
+    token_header = await get_admin_token_header(client, session)
     clean_skills = await setup_skill_data(session, 1)
     clean_skill = clean_skills[0]
     response = await client.post(
         f"{base_url}/",
         json={"skill_name": clean_skill.skill_name, "category": clean_skill.category},
+        headers=token_header,
     )
 
     assert response.status_code == 409
@@ -52,11 +58,13 @@ async def test_create_normal_skill_should_return_409_email_already_registered_PO
 
 @pytest.mark.anyio
 async def test_create_skill_should_return_409_email_already_registered_POST(client, session):
+    token_header = await get_admin_token_header(client, session)
     clean_skills = await setup_skill_data(session)
     clean_skill = clean_skills[0]
     response = await client.post(
         f"{base_url}/",
         json={"skill_name": clean_skill.skill_name, "category": clean_skill.category},
+        headers=token_header,
     )
 
     assert response.status_code == 409
@@ -64,15 +72,15 @@ async def test_create_skill_should_return_409_email_already_registered_POST(clie
 
 
 @pytest.mark.anyio
-async def test_get_all_skills_should_return_200_OK_GET(session, client):
+async def test_get_all_skills_should_return_200_OK_GET(session, client, default_search_options):
     clean_skills = await setup_skill_data(session=session, qty_size=8)
-    response = await client.get(f"{base_url}/?offset=0&limit=100")
+    response = await client.get(f"{base_url}/?{urlencode(default_search_options)}")
     response_json = response.json()
     response_founds = response_json["founds"]
 
     assert response.status_code == 200
     assert len(response_founds) == 8
-    assert response_json["search_options"] == {"limit": 100, "offset": 0, "total_count": 8}
+    assert response_json["search_options"] == default_search_options | {"total_count": 8}
     assert all(
         [
             skill.skill_name == response_founds[count]["skill_name"]
@@ -86,10 +94,10 @@ async def test_get_all_skills_should_return_200_OK_GET(session, client):
 
 
 @pytest.mark.anyio
-async def test_get_all_skills_with_limit_should_return_200_OK_GET(session, client):
-    limit = 5
+async def test_get_all_skills_with_page_size_should_return_200_OK_GET(session, client):
+    query_find_parameters = {"ordering": "created_at", "page": 1, "page_size": 5}
     clean_skills = await setup_skill_data(session, 5)
-    response = await client.get(f"{base_url}/?offset=0&limit={limit}")
+    response = await client.get(f"{base_url}/?{urlencode(query_find_parameters)}")
     response_json = response.json()
     response_founds = response_json["founds"]
 
@@ -98,44 +106,20 @@ async def test_get_all_skills_with_limit_should_return_200_OK_GET(session, clien
         [
             skill.skill_name == response_founds[count]["skill_name"]
             and skill.category == response_founds[count]["category"]
-            for count, skill in enumerate(clean_skills[:limit])
+            for count, skill in enumerate(clean_skills[: query_find_parameters["page_size"]])
         ]
     )
     assert len(response_founds) == 5
-    assert response_json["search_options"] == {"limit": limit, "offset": 0, "total_count": limit}
-    assert all([validate_datetime(skill["created_at"]) for skill in response_founds])
-    assert all([validate_datetime(skill["updated_at"]) for skill in response_founds])
-
-
-@pytest.mark.anyio
-async def test_get_all_skills_with_offset_should_return_200_OK_GET(session, client):
-    offset = 3
-    skills_qty_size = 5
-    clean_skills = await setup_skill_data(session=session, qty_size=skills_qty_size)
-    response = await client.get(f"{base_url}/?offset={offset}&limit=100")
-    response_json = response.json()
-    response_founds = response_json["founds"]
-
-    assert response.status_code == 200
-    assert all(
-        [
-            skill.skill_name == response_founds[count]["skill_name"]
-            and skill.category == response_founds[count]["category"]
-            for count, skill in enumerate(clean_skills[offset:])
-        ]
-    )
-    assert len(response_founds) == 2
-    assert response_json["search_options"] == {"limit": 100, "offset": offset, "total_count": 2}
+    assert response_json["search_options"] == query_find_parameters | {"total_count": 5}
     assert all([validate_datetime(skill["created_at"]) for skill in response_founds])
     assert all([validate_datetime(skill["updated_at"]) for skill in response_founds])
 
 
 @pytest.mark.anyio
 async def test_get_all_skills_with_pagination_should_return_200_OK_GET(session, client):
-    offset = 2
-    limit = 3
+    query_find_parameters = {"ordering": "created_at", "page": 2, "page_size": 4}
     clean_skills = await setup_skill_data(session=session, qty_size=8)
-    response = await client.get(f"{base_url}/?offset={offset}&limit={limit}")
+    response = await client.get(f"{base_url}/?{urlencode(query_find_parameters)}")
     response_json = response.json()
     response_founds = response_json["founds"]
 
@@ -144,30 +128,31 @@ async def test_get_all_skills_with_pagination_should_return_200_OK_GET(session, 
         [
             skill.skill_name == response_founds[count]["skill_name"]
             and skill.category == response_founds[count]["category"]
-            for count, skill in enumerate(clean_skills[offset : limit + offset])
+            for count, skill in enumerate(clean_skills[4:])
         ]
     )
-    assert len(response_founds) == limit
-    assert response_json["search_options"] == {"limit": limit, "offset": offset, "total_count": limit}
+    assert len(response_founds) == query_find_parameters["page_size"]
+    assert response_json["search_options"] == query_find_parameters | {
+        "total_count": query_find_parameters["page_size"]
+    }
     assert all([validate_datetime(skill["created_at"]) for skill in response_founds])
     assert all([validate_datetime(skill["updated_at"]) for skill in response_founds])
 
 
 @pytest.mark.anyio
 async def test_delete_skill_should_return_200_OK_DELETE(session, client):
-    _, auth_token = await token(client, session, normal_users=0, admin_users=1)
-    token_header = {"Authorization": f"Bearer {auth_token}"}
+    token_header = await get_admin_token_header(client, session)
     _ = await setup_skill_data(session=session, qty_size=1)
     skill = await get_skill_by_index(client, 0)
 
     response = await client.delete(f"{base_url}/{skill['id']}", headers=token_header)
     response_json = response.json()
-    get_users_response = await client.get(f"{base_url}/?offset=0&limit=100")
+    get_skills_response = await client.get(f"{base_url}/?offset=0&limit=100")
 
     assert response.status_code == 200
     assert response_json == {"detail": "Skill has been deleted successfully"}
-    assert get_users_response.status_code == 200
-    assert len(get_users_response.json()["founds"]) == 0
+    assert get_skills_response.status_code == 200
+    assert len(get_skills_response.json()["founds"]) == 0
 
 
 @pytest.mark.anyio
@@ -179,12 +164,12 @@ async def test_delete_skill_should_return_403_FORBIDDEN_DELETE(session, client):
 
     response = await client.delete(f"{base_url}/{skill['id']}", headers=token_header)
     response_json = response.json()
-    get_users_response = await client.get(f"{base_url}/?offset=0&limit=100")
+    get_skills_response = await client.get(f"{base_url}/?offset=0&limit=100")
 
     assert response.status_code == 403
     assert response_json == {"detail": "Not enough permissions"}
-    assert get_users_response.status_code == 200
-    assert len(get_users_response.json()["founds"]) == 1
+    assert get_skills_response.status_code == 200
+    assert len(get_skills_response.json()["founds"]) == 1
 
 
 @pytest.mark.anyio
@@ -214,45 +199,3 @@ async def test_get_by_id_should_return_404_NOT_FOUND_GET(session, client):
 
 
 ic
-# @pytest.mark.anyio
-# async def test_delete_different_user_should_return_403_FORBIDDEN_DELETE(session, client):
-#     _, auth_token = await token(client, session, normal_users=2, clean_user_index=0)
-#     token_header = {"Authorization": f"Bearer {auth_token}"}
-#     other_user = await get_user_by_index(client, 1)
-
-#     response = await client.delete(f"{base_url}/{other_user['id']}", headers=token_header)
-#     response_json = response.json()
-
-#     assert response.status_code == 403
-#     assert response_json == {"detail": "Not enough permissions"}
-
-
-# ic
-
-
-# from app.schemas.skill_schema import BaseSkill
-# from tests.factories import SkillFactory
-# from tests.routes.v1.base_route_test import BaseTest
-
-
-# class TestSkill(BaseTest):
-#     def __init__(-> None:
-#         # base_url, setup_func, base_schema, model_factory, model_name, id_type
-#         super().__init__(
-#             base_url=base_url,
-#             setup_func=setup_skill_data,
-#             base_schema=BaseSkill,
-#             model_factory=SkillFactory,
-#             model_name="Skills",
-#             id_type=int,
-#             base_input_keys=["skill_name", "category"]
-#         )
-
-# @pytest.mark.anyio
-# @pytest.mark.parametrize("test_class", [TestSkill])
-# async def test_skill_parametrized(test_class, client, session):
-#     # Crie uma instância da classe de teste e execute seus métodos de teste
-#     test_instance = test_class()
-#     await test_instance.test_create_should_return_201_POST(client, session)
-#     await test_instance.test_create_should_return_422_unprocessable_entity_POST(client)
-#     await test_instance.test_create_should_return_409_email_already_registered_POST(client, session)
