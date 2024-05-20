7d5fdb3eece8b8f44149ac36fa11a8325a9bcab6
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-05-20 16:12:26 -0300

    feat(Models): Adding the userskills association to the init import package of models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 from .api_models import Skill
 from .api_models import User
+from .api_models import UserSkillsAssociation
 from .base_model import Base
 
-__all__ = ["User", "Skill", "Base"]
+__all__ = ["User", "Skill", "Base", "UserSkillsAssociation"]
