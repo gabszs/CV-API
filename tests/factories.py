714ffb9fa1496b2d4b4c301805f60112c276f7a1
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-06-05 01:49:18 -0300

    test(Tests): Adding the helpers.py, this file will include all the helper functions to all the tests folders and also fixture, by adoptiong this, we move all the non-fixture functions from conftest to a better and clear helpers file
    alsso refacatoring the factories to new user model and refactorin the create_factory-users

--- tests/factories.py
+++ tests/factories.py
@@ -1,18 +1,16 @@
 from typing import Any
 from typing import Dict
-from typing import List
 
 import factory
 from factory import fuzzy
 from factory.base import StubObject
 
-from app.core.security import get_password_hash
 from app.models import Skill
 from app.models import User
 from app.models import UserSkillsAssociation
 from app.models.models_enums import CategoryOptions
 from app.models.models_enums import SkillLevel
-from app.schemas.user_schema import UserWithCleanPassword
+from app.models.models_enums import UserRoles
 
 
 def convert_dict_from_stub(stub: StubObject) -> Dict[str, Any]:
@@ -25,7 +23,6 @@ def convert_dict_from_stub(stub: StubObject) -> Dict[str, Any]:
 
 def factory_object_to_dict(factory_instance):
     attributes = factory_instance.__dict__
-    # Remova atributos internos que não são relevantes para o dicionário
     attributes.pop("_declarations", None)
     return attributes
 
@@ -37,8 +34,8 @@ class UserFactory(factory.Factory):
     username = factory.Sequence(lambda x: f"user_{x}")
     email = factory.LazyAttribute(lambda x: f"{x.username}@test.com")
     password = factory.LazyAttribute(lambda obj: f"{obj.username}_password")
+    role = UserRoles.BASE_USER
     is_active = None
-    is_superuser = None
 
 
 class SkillFactory(factory.Factory):
@@ -61,42 +58,5 @@ class UserSkillFactory(factory.Factory):
     user = None
 
 
-def create_factory_users(users_qty: int, **kwargs) -> Dict[UserFactory, str]:
-    users_dict: Dict[Dict[str, List[UserFactory]], Dict[str, List[str]]] = {"users": list(), "clean_users": list()}
-    for _ in range(users_qty):
-        user = UserFactory(**kwargs)
-        clean_password = user.password
-        user.password = get_password_hash(user.password)
-
-        clean_user = UserWithCleanPassword(
-            email=user.email, username=user.username, password=user.password, clean_password=clean_password
-        )
-
-        users_dict["users"].append(user)
-        users_dict["clean_users"].append(clean_user)
-    return users_dict
-
-
-def batch_users_by_options(
-    normal_users: int = 0, admin_users: int = 0, disable_users: int = 0, disable_admins: int = 0
-):
-    normal_users = create_factory_users(normal_users)
-    admin_users = create_factory_users(admin_users, is_superuser=True)
-    disable_users = create_factory_users(disable_users, is_active=False)
-    disable_admins = create_factory_users(disable_admins, is_active=False, is_superuser=True)
-    users = [
-        user
-        for user_list in [normal_users["users"], admin_users["users"], disable_users["users"], disable_admins["users"]]
-        for user in user_list
-    ]
-    clean_users = [
-        user
-        for user_list in [
-            normal_users["clean_users"],
-            admin_users["clean_users"],
-            disable_users["clean_users"],
-            disable_admins["clean_users"],
-        ]
-        for user in user_list
-    ]
-    return users, clean_users
+def create_factory_users(users_qty: int = 1, user_role: UserRoles = UserRoles.BASE_USER, is_active=True):
+    return UserFactory.create_batch(users_qty, role=user_role, is_active=is_active)
---
+++ tests/helpers.py
@@ -0,0 +1,118 @@
+from datetime import datetime
+from typing import List
+from typing import Optional
+
+from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.security import get_password_hash
+from app.core.settings import settings
+from app.models.models_enums import UserRoles
+from tests.factories import create_factory_users
+from tests.factories import SkillFactory
+from tests.factories import UserFactory
+from tests.schemas import TestSkillSchema
+from tests.schemas import UserModelSetup
+from tests.schemas import UserSchemaWithHashedPassword
+
+
+def validate_datetime(data_string):
+    try:
+        datetime.strptime(data_string, "%Y-%m-%dT%H:%M:%S.%fZ")
+        return True
+    except ValueError:
+        try:
+            datetime.strptime(data_string, "%Y-%m-%dT%H:%M:%S")
+            return True
+        except ValueError:
+            return False
+
+
+async def get_skill_by_index(client, index: int = 0, token_header: Optional[str] = None):
+    response = await client.get(f"{settings.base_skill_url}/?ordering=id", headers=token_header)
+    return response.json()["founds"][index]
+
+
+async def get_user_by_index(client, index: int = 0, token_header: Optional[str] = None):
+    response = await client.get(f"{settings.base_users_url}/?ordering=username", headers=token_header)
+    return response.json()["founds"][index]
+
+
+async def add_users_models(
+    session: AsyncSession,
+    users_qty: int = 1,
+    user_role: UserRoles = UserRoles.BASE_USER,
+    is_active=True,
+    index: Optional[int] = None,
+) -> List[UserFactory]:
+    return_users: List[UserFactory] = []
+    users = create_factory_users(users_qty=users_qty, user_role=user_role, is_active=is_active)
+    password_list = [factory_model.password for factory_model in users]
+    for user in users:
+        user.password = get_password_hash(user.password)
+    session.add_all(users)
+    await session.commit()
+    for count, user in enumerate(users):
+        await session.refresh(user)
+        return_users.append(
+            UserSchemaWithHashedPassword(
+                id=user.id,
+                created_at=user.created_at,
+                updated_at=user.updated_at,
+                email=user.email,
+                username=user.username,
+                is_active=user.is_active,
+                role=user.role,
+                password=password_list[count],
+                hashed_password=user.password,
+            )
+        )
+    if index is not None:
+        return return_users[index]
+    return return_users
+
+
+async def setup_users_data(session, model_args: List[UserModelSetup]) -> List[UserSchemaWithHashedPassword]:
+    return_list: List[UserSchemaWithHashedPassword] = []
+    for user_setup in model_args:
+        user_list = await add_users_models(
+            session, users_qty=user_setup.qty, user_role=user_setup.role, is_active=user_setup.is_active
+        )
+        return_list.append(*user_list)
+    return return_list
+
+
+async def token(client, session, base_auth_route: str = "/v1/auth", **kwargs):
+    user = await add_users_models(session=session, index=0, **kwargs)
+    response = await client.post(
+        f"{base_auth_route}/sign-in", json={"email__eq": user.email, "password": user.password}
+    )
+    return {"Authorization": f"Bearer {response.json()['access_token']}"}
+
+
+async def get_user_token(client: AsyncClient, user: UserSchemaWithHashedPassword) -> str:
+    response = await client.post(
+        f"{settings.base_auth_route}/sign-in", json={"email__eq": user.email, "password": user.password}
+    )
+    return {"Authorization": f"Bearer {response.json()['access_token']}"}
+
+
+async def setup_skill_data(session, skills_qty: int = 1, index: Optional[int] = None) -> List[TestSkillSchema]:
+    return_list: List[TestSkillSchema] = []
+    skills = SkillFactory.create_batch(skills_qty)
+    session.add_all(skills)
+    await session.commit()
+    for skill in skills:
+        await session.refresh(skill)
+        return_list.append(
+            TestSkillSchema(
+                id=skill.id,
+                skill_name=skill.skill_name,
+                category=skill.category,
+                created_at=skill.created_at,
+                updated_at=skill.updated_at,
+            )
+        )
+    if index is not None:
+        return return_list[index]
+    return return_list
