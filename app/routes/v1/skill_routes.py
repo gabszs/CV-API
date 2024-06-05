74bd1aa179647f86760a912af8497daec1927969
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-06-05 01:41:08 -0300

    feat(Routes): Implemetning the authorization wrapper in the users and skills enpoint based on explicity roles, alsso adding allow_same_id in necessary endpoints

--- app/routes/v1/skill_routes.py
+++ app/routes/v1/skill_routes.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
-from app.core.dependencies import CurrentSuperUserDependency
+from app.core.dependencies import CurrentUserDependency
 from app.core.dependencies import FindQueryParameters
 from app.core.dependencies import SkillServiceDependency
+from app.core.security import authorize
 from app.models.models_enums import CategoryOptions
+from app.models.models_enums import UserRoles
 from app.schemas.base_schema import Message
 from app.schemas.skill_schema import BaseSkill
 from app.schemas.skill_schema import FindSkillResult
@@ -24,32 +26,37 @@ async def get_skill_by_id(skill_id: int, service: SkillServiceDependency):
 
 
 @router.post("/", status_code=201, response_model=PublicSkill)
-async def create_skill(skill: BaseSkill, service: SkillServiceDependency, current_user: CurrentSuperUserDependency):
+@authorize(role=[UserRoles.ADMIN, UserRoles.MODERATOR, UserRoles.BASE_USER])
+async def create_skill(skill: BaseSkill, service: SkillServiceDependency, current_user: CurrentUserDependency):
     return await service.add(skill)
 
 
 @router.put("/{skill_id}", response_model=PublicSkill)
+@authorize(role=[UserRoles.ADMIN, UserRoles.MODERATOR])
 async def update_skill(
-    skill_id: int, skill: UpdateSkill, service: SkillServiceDependency, current_user: CurrentSuperUserDependency
+    skill_id: int, skill: UpdateSkill, service: SkillServiceDependency, current_user: CurrentUserDependency
 ):
     return await service.patch(id=skill_id, schema=skill)
 
 
 @router.patch("/{skill_id}/category/{category}", response_model=PublicSkill)
+@authorize(role=[UserRoles.ADMIN, UserRoles.MODERATOR])
 async def change_skill_category(
-    skill_id: int, category: CategoryOptions, service: SkillServiceDependency, current_user: CurrentSuperUserDependency
+    skill_id: int, category: CategoryOptions, service: SkillServiceDependency, current_user: CurrentUserDependency
 ):
     return await service.patch_attr(id=skill_id, attr="category", value=category)
 
 
 @router.patch("/{skill_id}/skill_name/{skill_name}", response_model=PublicSkill)
+@authorize(role=[UserRoles.ADMIN, UserRoles.MODERATOR])
 async def change_skill_skill_name(
-    skill_id: int, skill_name: str, service: SkillServiceDependency, current_user: CurrentSuperUserDependency
+    skill_id: int, skill_name: str, service: SkillServiceDependency, current_user: CurrentUserDependency
 ):
     return await service.patch_attr(id=skill_id, attr="skill_name", value=skill_name)
 
 
 @router.delete("/{skill_id}", response_model=Message)
-async def delete_skill(skill_id: int, service: SkillServiceDependency, current_user: CurrentSuperUserDependency):
+@authorize(role=[UserRoles.ADMIN, UserRoles.MODERATOR])
+async def delete_skill(skill_id: int, service: SkillServiceDependency, current_user: CurrentUserDependency):
     await service.remove_by_id(id=skill_id)
     return Message(detail="Skill has been deleted successfully")
--- app/routes/v1/users_routes.py
+++ app/routes/v1/users_routes.py
@@ -3,10 +3,11 @@ from uuid import UUID
 from fastapi import APIRouter
 from fastapi import Depends
 
-from app.core.dependencies import CurrentSuperUserDependency
 from app.core.dependencies import CurrentUserDependency
 from app.core.dependencies import FindBase
 from app.core.dependencies import UserServiceDependency
+from app.core.security import authorize
+from app.models.models_enums import UserRoles
 from app.schemas.base_schema import Message
 from app.schemas.user_schema import BaseUserWithPassword
 from app.schemas.user_schema import FindUserResult
@@ -17,12 +18,16 @@ router = APIRouter(prefix="/user", tags=["user"])
 
 
 @router.get("/", response_model=FindUserResult)
-async def get_user_list(service: UserServiceDependency, find_query: FindBase = Depends()):
+@authorize(role=[UserRoles.MODERATOR, UserRoles.ADMIN])
+async def get_user_list(
+    service: UserServiceDependency, current_user: CurrentUserDependency, find_query: FindBase = Depends()
+):
     return await service.get_list(find_query)
 
 
 @router.get("/{user_id}", response_model=User)
-async def get_user_by_id(user_id: UUID, service: UserServiceDependency):
+@authorize(role=[UserRoles.MODERATOR, UserRoles.ADMIN], allow_same_id=True)
+async def get_user_by_id(user_id: UUID, service: UserServiceDependency, current_user: CurrentUserDependency):
     return await service.get_by_id(user_id)
 
 
@@ -31,33 +36,31 @@ async def create_user(user: BaseUserWithPassword, service: UserServiceDependency
     return await service.add(user)
 
 
+### importante tem de fazer
 ### adicionar validacao para quano o a request tiver parametros iguais ao do current_user
 @router.put("/{user_id}", response_model=User)
+@authorize(role=[UserRoles.MODERATOR, UserRoles.ADMIN], allow_same_id=True)
 async def update_user(
-    user_id: UUID, user: UpsertUser, service: UserServiceDependency, current_user: CurrentSuperUserDependency
+    user_id: UUID, user: UpsertUser, service: UserServiceDependency, current_user: CurrentUserDependency
 ):
     return await service.patch(id=user_id, schema=user, current_user=current_user)
 
 
 @router.patch("/enable_user/{user_id}", response_model=Message)
+@authorize(role=[UserRoles.MODERATOR, UserRoles.ADMIN], allow_same_id=True)
 async def enabled_user(user_id: UUID, service: UserServiceDependency, current_user: CurrentUserDependency):
     await service.patch_attr(id=user_id, attr="is_active", value=True, current_user=current_user)
     return Message(detail="User has been enabled successfully")
 
 
 @router.delete("/disable/{user_id}", response_model=Message)
+@authorize(role=[UserRoles.MODERATOR, UserRoles.ADMIN], allow_same_id=True)
 async def disable_user(user_id: UUID, service: UserServiceDependency, current_user: CurrentUserDependency):
     await service.patch_attr(id=user_id, attr="is_active", value=False, current_user=current_user)
     return Message(detail="User has been desabled successfully")
 
 
-@router.delete("/{user_id}", response_model=Message)
+@router.delete("/{user_id}", status_code=204)
+@authorize(role=[UserRoles.ADMIN])
 async def delete_user(user_id: UUID, service: UserServiceDependency, current_user: CurrentUserDependency):
-    await service.remove_by_id(user_id, current_user=current_user)
-    return Message(detail="User has been deleted successfully")
-
-
-# @router.patch("/{user_id}", response_model=User)
-# async def change_user_password(user_id: UUID, password: str, roservice: UserServiceDependency, current_user: CurrentUserDependency):
-#     pass
-# 3fa85f64-5717-4562-b3fc-2c963f66afa6
+    await service.remove_by_id(user_id)
