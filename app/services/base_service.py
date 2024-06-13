693e454f254d14f4519117285a404498a55997ca
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-06-13 17:13:47 -0300

    feat(BaseService): Implments kwargs in all base_service methods

--- app/services/base_service.py
+++ app/services/base_service.py
@@ -19,23 +19,23 @@ class BaseService:
             if id != current_user.id:
                 raise AuthError(detail="Not enough permissions")
 
-    async def get_list(self, schema: FindBase):
-        return await self._repository.read_by_options(schema)
+    async def get_list(self, schema: FindBase, **kwargs):
+        return await self._repository.read_by_options(schema, **kwargs)
 
-    async def get_by_id(self, id: Union[UUID, int]):
-        return await self._repository.read_by_id(id)
+    async def get_by_id(self, id: Union[UUID, int], **kwargs):
+        return await self._repository.read_by_id(id, **kwargs)
 
-    async def add(self, schema):
-        return await self._repository.create(schema)
+    async def add(self, schema, **kwargs):
+        return await self._repository.create(schema, **kwargs)
 
-    async def patch(self, id: Union[UUID, int], schema):
-        return await self._repository.update(id, schema)
+    async def patch(self, id: Union[UUID, int], schema, **kwargs):
+        return await self._repository.update(id, schema, **kwargs)
 
-    async def patch_attr(self, id: Union[UUID, int], attr: str, value):
-        return await self._repository.update_attr(id, attr, value)
+    async def patch_attr(self, id: Union[UUID, int], attr: str, value, **kwargs):
+        return await self._repository.update_attr(id, attr, value, **kwargs)
 
-    async def put_update(self, id: Union[UUID, int], schema):
-        return await self._repository.whole_update(id, schema)
+    async def put_update(self, id: Union[UUID, int], schema, **kwargs):
+        return await self._repository.whole_update(id, schema, **kwargs)
 
-    async def remove_by_id(self, id: Union[UUID, int]):
-        return await self._repository.delete_by_id(id)
+    async def remove_by_id(self, id: Union[UUID, int], **kwargs):
+        return await self._repository.delete_by_id(id, **kwargs)
